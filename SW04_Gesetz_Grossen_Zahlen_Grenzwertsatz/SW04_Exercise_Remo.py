133777b86d2fd6861c4b3671bbb5e53d625462c8
Remo Schwarzentruber <Remo.Schwarzentruber@stud.hslu.ch>
2019-01-22 17:21:37 +0100

    SW04 - finished

--- SW04_Gesetz_Grossen_Zahlen_Grenzwertsatz/SW04_Exercise_Remo.py
+++ SW04_Gesetz_Grossen_Zahlen_Grenzwertsatz/SW04_Exercise_Remo.py
@@ -128,5 +128,44 @@ plt.show()
 # =============================================================================
 norm.cdf(x=95, loc=100, scale=2)
 
+# =============================================================================
+# Aufgabe 4.4
+# =============================================================================
+
+# b)
+norm.cdf(x=2, loc=1, scale=2) - norm.cdf(x=0, loc=1, scale=2)
+
+# c)
+norm.cdf(x=51, loc=50, scale=np.sqrt(200)) - norm.cdf(x=49, loc=50, scale=np.sqrt(200))
+
+# d)
+norm.cdf(x=2, loc=1, scale=np.sqrt(2/25)) - norm.cdf(x=0, loc=1, scale=np.sqrt(2/25))
 
 
+
+# =============================================================================
+# Aufgabe 4.5
+# =============================================================================
+
+a = [79.98, 80.04, 80.02, 80.04, 80.03, 80.03, 80.04, 79.97, 80.05, 80.03,
+     80.02, 80.00, 80.02]
+b = [80.02, 79.94, 79.98, 79.97, 79.97, 80.03, 79.95, 80.03, 79.95, 79.97]
+
+# a)
+mean_a = np.mean(a)
+mean_b = np.mean(b)
+se_a = np.std(a) / np.sqrt(len(a))
+se_b = np.std(b) / np.sqrt(len(b))
+print('μa={:.4f}'.format(mean_a))
+print('σa={:.4f}'.format(se_a))
+print('μb={:.4f}'.format(mean_b))
+print('σb={:.4f}'.format(se_b))
+
+print('Methode A: ({:.3f}±{:.3f})cal/g'.format(mean_a, se_a))
+print('Methode B: ({:.3f}±{:.3f})cal/g'.format(mean_b, se_b))
+
+# b)
+ser_a = se_a / mean_a
+ser_b = se_b / mean_b
+print('Methode A: {:.3f}cal/g ±{:.3f}%'.format(mean_a, ser_a*100))
+print('Methode B: {:.3f}cal/g ±{:.3f}%'.format(mean_b, ser_b*100))
