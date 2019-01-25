14b525661157fd70dc8a9e1bad2d9740450ec2b2
Remo Schwarzentruber <Remo.Schwarzentruber@stud.hslu.ch>
2019-01-25 07:34:39 +0100

    add SW05

---
+++ SW05_QQ-Plots_Parameterschätzung/SW05_Book_References.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+
+import matplotlib.pyplot as plt
+from scipy.stats import norm, binom
+import numpy as np
+import pandas as pd
+import scipy.stats as st
+from pandas import Series
+import matplotlib.mlab as mlab
+
+# =============================================================================
+# Q-Q-Plot
+# -> muehsam 
+# =============================================================================
+x = Series([24.4, 27.6, 27.8, 27.9, 28.5, 30.1, 30.3, 31.7, 32.2, 32.8, 33.3, 33.5, 34.1, 34.6, 35.8, 35.9, 36.8, 37.1, 39.2, 39.7])
+
+alphak = (np.arange(1, x.size + 1) - 0.5) / x.size
+
+quantile_theor = norm.ppf(q = alphak, loc = x.mean(), scale = x.std())
+quantile_empir = np.sort(x)
+
+
+plt.xlabel("Theor. Quantile")
+plt.ylabel("Empirische Quantile")
+plt.plot(quantile_theor, quantile_empir, "o")
+plt.show()
+
+
+# =============================================================================
+# Q-Q-Plot
+# -> Einfach
+# =============================================================================
+x = Series([24.4, 27.6, 27.8, 27.9, 28.5, 30.1, 30.3, 31.7, 32.2, 32.8, 33.3, 33.5, 34.1, 34.6, 35.8, 35.9, 36.8, 37.1, 39.2, 39.7])
+st.probplot(x, plot=plt)
+
+
+# =============================================================================
+# Parameterschätzung für stetige Wahrscheinlichkeitsverteilungen
+# Momentenmethode
+# =============================================================================
+
+test = pd.Series([11.96, 5.03, 67.40, 16.07, 31.50, 7.73, 11.10,22.38])
+test.describe()
+
+LamdaGeschaetzt = 1 / test.mean()
+print(LamdaGeschaetzt)
+
+# =============================================================================
+# Parameterschätzung für stetige Wahrscheinlichkeitsverteilungen
+# Maximum-Likelihood-Methode
+# =============================================================================
+st.binom.pmf(k=58, n=100, p=0.5)
+st.binom.pmf(k=58, n=100, p=0.6)
+
--- SW05_QQ-Plots_Parameterschätzung/SW05_Exercise_Remo.py
+++ SW05_QQ-Plots_Parameterschätzung/SW05_Exercise_Remo.py
@@ -14,7 +14,69 @@ import numpy as np
 # =============================================================================
 # Import
 # =============================================================================
-iron = pd.read_table("../data/ironF3.dat",sep=" ",index_col=False)
+iron = pd.read_table("data/ironF3.dat",sep=" ",index_col=False)
+
+
+# =============================================================================
+# Aufgabe 5.1
+# =============================================================================
+# a)
+plt.figure(1)
+plt.subplot(2,2,1)
+x = st.norm.rvs(size=10)
+st.probplot(x, plot=plt)
+plt.subplot(2,2,2)
+x = st.norm.rvs(size=20)
+st.probplot(x, plot=plt)
+plt.subplot(2,2,3)
+x = st.norm.rvs(size=50)
+st.probplot(x, plot=plt)
+plt.subplot(2,2,4)
+x = st.norm.rvs(size=100)
+st.probplot(x, plot=plt)
+plt.tight_layout()
+plt.show()
+
+# b)
+for i in range(1,4):
+    plt.subplot(1,3,i)
+    x = st.t.rvs(size=20, df=1)
+    st.probplot(x,plot=plt)
+    plt.title("n=20,df=1")
+    
+for i in range(1,4):
+    plt.subplot(1,3,i)
+    x = st.t.rvs(size=100, df=1)
+    st.probplot(x,plot=plt)
+    plt.title("n=20,df=1")
+
+# c)
+for i in range(1,4):
+    plt.subplot(1,3,i)
+    x = st.chi2.rvs(size=10, df=20)
+    st.probplot(x,plot=plt)
+    plt.title("n=20,df=20")
+
+# =============================================================================
+# Aufgabe 5.2
+# =============================================================================
+# a)
+# mögliche Werte
+werte = np.array([0,10,11])
+
+# X simulieren
+sim = pd.Series(np.random.choice(werte, size=1000, replace=True))
+
+# Plot
+plt.subplot(4,2,1)
+
+sim.hist(bins=[0,1,10,11,12], edgecolor="black")
+plt.title("Original")
+
+plt.subplot(4,2,2)
+st.probplot(sim, plot=plt)
+plt.title("Normal Q-Q Plot")
+
 
 # =============================================================================
 # Aufgabe 5.3
