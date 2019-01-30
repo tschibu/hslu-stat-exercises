0a6c65cb21a47b12056f9d8e503e9370cc5d688e
Remo Schwarzentruber <Remo.Schwarzentruber@stud.hslu.ch>
2019-01-30 06:14:36 +0100

    finished Block 13 (Random Walk / Stochastischer Prozess)

--- SW13_Zeitreihen/SW13_Excercies_REMO.py
+++ SW13_Zeitreihen/SW13_Excercies_REMO.py
@@ -18,29 +18,40 @@ d = np.random.choice(a=[-1,1], size = 1000, replace=True)
 x = np.cumsum(d)
 plt.plot(x)
 
+# b)
+# mit pi=0.5 => np.sqrt(N)
+np.sqrt(1000)
 
-help(np.cumsum)
+## Allgemeint Funktion:
+pi=0.5
+N=1000
+np.sqrt(4*(N*pi**2+N**2*pi**2)+N**2-4*N**2*pi)
 
 
 
 # =============================================================================
 # Aufgabe d)
 # =============================================================================
-sp2012 = pd.read_table('data/sp2012.txt')
+sp2012 = pd.read_table('data/sp2012.txt', header=None)
+sp2012.columns=['sp2012']
 df = pd.DataFrame(sp2012)
 
 plt.plot(df)
 plt.xlabel("Zeit (Tage)")
 plt.ylabel("Wert")
 plt.title('S&P 500 - Aktienkurs 2012')
-
 plt.show()
 
+df.head(1)
+df.tail(1)
+df["sp2012"].describe()
 
 
-steps = np.array(st.norm.rvs(size=250, loc=0.483, scale=11))
+m=0.483
+s=11
+steps = np.array(st.norm.rvs(size=250, loc=m, scale=s))
 sp_simulated = np.empty([250])
-sp_simulated[0] = -1257.7
+sp_simulated[0] = 1257.7
 
 for i in range(249):
     sp_simulated[i+1] = sp_simulated[i]+ steps[i]
@@ -56,10 +67,13 @@ plt.show()
 # =============================================================================
 # Aufgabe 13.3
 # =============================================================================
+# a)
 2.5 / np.exp(-np.log(2))
 
-np.exp(-np.log(2))
+# b)
+1 - st.norm.cdf(x=1, loc=5, scale=2)
+st.norm.sf(x=1, loc=5, scale=2)
 
-10 / np.exp(-np.log(0.5))
-
-
+# e)
+np.exp(-np.log(0.5))
+np.exp(np.log(0.5))
